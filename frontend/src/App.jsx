28e68996f53585d7f4b8c69e037d812e2076e692
vicky raj <rajv60701@gmail.com>
2025-06-04 21:09:51 +0530

    frontEnd setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
-
+import { Route,Routes,BrowserRouter} from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import CreateNote from "./pages/CreateNote";
+import NoteByid from "./pages/NoteByid";
 function App() {
   const fetchNotes = async()=>{
     const notes = await axios.get("http://localhost:3000/api/notes")
@@ -15,7 +18,16 @@ useEffect(() => {
 
   return(
 
-    <div>hi</div>
+    <div>
+      <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Homepage/>}></Route>
+        <Route path="/create" element={<CreateNote/>}></Route>
+        <Route path="/note/:id" element={<NoteByid/>}></Route>
+      </Routes>
+      </BrowserRouter>
+    
+    </div>
   )
 }
 
---
+++ frontend/src/pages/NoteByid.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoteByid = () => {
+  return (
+    <div>NoteByid</div>
+  )
+}
+
+export default NoteByid
